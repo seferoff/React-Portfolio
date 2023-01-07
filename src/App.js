e04a4fadc1286a5983e48e9de95e5c6f7fe7b287
seferoff <seferoff007@gmail.com>
2023-01-07 17:13:33 +0400

    ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Home from "./pages/HomePage/Home.js"
+import About from "./pages/AboutPage/About.js"
+import Experience from "./pages/ExperiencePage/Experience.js"
+import Contact from "./pages/ContactPage/Contact.js"
+import NotFound from "./pages/NotFoundPage/NotFound.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/experience">Experience</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <main>
+     <Routes>
+     <Route path="/" element={<Home/>}></Route>
+     <Route path="/about" element={<About/>}></Route>
+     <Route path="/experience" element={<Experience/>}></Route>
+     <Route path="/contact" element={<Contact/>}></Route>
+     <Route path="/*" element={<NotFound/>}></Route>
+     </Routes>
+   </main>
     </div>
   );
 }
---
+++ src/pages/NotFoundPage/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./NotFound.css"
+
+const NotFound = () => {
+  return (
+    <div className='notFound'>Sorgunuza uygun bir sehife yoxdur.</div>
+  )
+}
+
+export default NotFound
